202222ed362a5e9b1273439141b5f77fc471ebb9
Michael Schmidt <6544930+mjschmidt@users.noreply.github.com>
2019-03-24 16:23:33 -0400

    updated docker run command, now has git

--- run-kube-shell.sh
+++ run-kube-shell.sh
@@ -1,1 +1,1 @@
-docker run --name my-kube-shell -v /home/$USER/.kube/config:/config -it orchapod/kubeshell:0.0.1-alpine ash
+docker run --name my-kube-shell -v /home/$USER/.kube/config:/config -it mjschmidt/kubeshell:0.0.2-alpine ash
